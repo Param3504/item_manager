a7defc17edcd4659e385a279a44471578dc7fb3d
Param Bakulbhai Prajapati <paramprajapati7570@gmail.com>
2025-01-11 16:08:18 +0530

    Updates

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-// import './index.css'
-import App from './App.jsx'
-import ItemListManager from './ans.jsx';
-
-createRoot(document.getElementById("root")).render(
-  <StrictMode><ItemListManager /></StrictMode>
-);
